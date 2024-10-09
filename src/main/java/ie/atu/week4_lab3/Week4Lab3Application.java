200415555ea11c7698b682a813e1b48b7b3411c7
Shine Sujith <G00414454@atu.ie>
2024-10-09 14:03:25 +0100

    project created

---
+++ src/main/java/ie/atu/week4_lab3/Week4Lab3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week4_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4Lab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week4_lab3/Week4Lab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week4_lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4Lab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
