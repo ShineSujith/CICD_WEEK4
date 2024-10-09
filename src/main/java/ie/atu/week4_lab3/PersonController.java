ebe54b43d2b79e50ed662218a7192c135c3da704
Shine Sujith <G00414454@atu.ie>
2024-10-09 14:32:45 +0100

    added a method to create a new person and another method to display everyone in the person list

---
+++ src/main/java/ie/atu/week4_lab3/PersonController.java
@@ -0,0 +1,24 @@
+package ie.atu.week4_lab3;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    List<Person> personList = new ArrayList<>();
+
+    @GetMapping("/getPerson")
+    public List<Person> getPerson() {
+        return personList;
+    }
+    @PostMapping("/createPerson")
+    public List<Person> addPerson(@Valid @RequestBody Person person) {
+        personList.add(person);
+        return personList;
+    }
+
+}
